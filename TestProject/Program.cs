c09be0a24f3ca7589d83e1ba923882a387fc8ead
Rodrigo Souza <rodrigofs23@hotmail.com>
2023-12-06 05:05:21 -0300

    Add logger

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,9 +1,25 @@
+using Work.Database;
+using Work.Implementation;
+using Work.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Register services for DI
+builder.Services.AddScoped<IRepository<User, Guid>>(provider =>
+{
+    var mockDatabase = new MockDatabase(seed: 10);
+    return new UserRepository(mockDatabase);
+});
+
+//builder.Services.AddSingleton<IRepository<User, Guid>, UserRepository>();
+
+//builder.Services.AddScoped<UserRepository, IRepository>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
