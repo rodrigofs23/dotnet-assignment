29babdeba41c7e2b84ecfe6c1fe763d89e5ff68c
Rodrigo Souza <rodrigofs23@hotmail.com>
2023-12-06 05:03:11 -0300

    Add response correct codes

--- TestProject/Controllers/UserController.cs
+++ TestProject/Controllers/UserController.cs
@@ -1,89 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Work.ApiModels;
-using Work.Database;
-using Work.Interfaces;
 
 namespace Work.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
 
-        private readonly ILogger<UserController> _logger;
-        private readonly IRepository<User, Guid> _userRepository;
-
-        public UserController(ILogger<UserController> logger, IRepository<User, Guid> userRepository)
-        {
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
-        }
-
-        [HttpGet("{id}")]
         public IActionResult Get(Guid id)
         {
-            var user = _userRepository.Read(id);
-            if (user == null)
-            {
-                _logger.LogInformation("User with ID {UserId} not found", id);
-                return NotFound();
-            }
-            return Ok(user);
+            throw new NotImplementedException("TODO");
         }
 
-        [HttpPost]
-        public IActionResult Post(UserModelDto userModelDto)
+        public IActionResult Post(UserModelDto user)
         {
-            try
-            {
-                var newUser = new User
-                {
-                    UserId = Guid.NewGuid(),
-                    UserName = userModelDto.Name,
-                    Birthday = userModelDto.Birthdate
-                };
-                _userRepository.Create(newUser);
-                return CreatedAtAction(nameof(Get), new { id = newUser.UserId }, newUser);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error creating user");
-                return StatusCode(500, ex.Message);
-            }
+            throw new NotImplementedException("TODO");
         }
-
-        [HttpPut]
-        public IActionResult Put(UserModelDto userModelDto)
+        
+        public IActionResult Put(UserModelDto user)
         {
-            var existingUser = _userRepository.Read(userModelDto.Id);
-            if (existingUser == null)
-            {
-                return NotFound();
-            }
-            existingUser.UserName = userModelDto.Name;
-            existingUser.Birthday = userModelDto.Birthdate;
-            _userRepository.Update(existingUser);
-            return NoContent();
+            throw new NotImplementedException("TODO");
         }
 
-        [HttpDelete("{id}")]
         public IActionResult Delete(Guid id)
         {
-            try
-            {
-                var userToDelete = _userRepository.Read(id);
-                if (userToDelete == null)
-                {
-                    return NotFound();
-                }
-                _userRepository.Remove(userToDelete);
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error deleting user with ID {UserId}", id);
-                return StatusCode(500, ex.Message);
-            }
+            throw new NotImplementedException("TODO");
         }
 
     }
